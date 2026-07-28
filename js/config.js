abc75ad788acd9ec8d5dad84e3ca7376234e7993
Zafod <70067552+zafodtom@users.noreply.github.com>
2026-07-28 17:44:42 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,13 @@
+window.AISHIELD_CONFIG = {
+  email: "kontakt@aishield.cz",
+  phoneDisplay: "+420 XXX XXX XXX",
+  phoneLink: "+420000000000",
+  companyId: "doplnit",
+
+  /*
+   * Volitelné:
+   * Vložte URL služby pro zpracování formuláře, například Formspree.
+   * Pokud zůstane prázdná, formulář otevře uživatelův e-mailový klient.
+   */
+  formEndpoint: ""
+};
